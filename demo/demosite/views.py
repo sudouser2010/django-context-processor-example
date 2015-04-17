7f4d267e4cee57c02d21b4bdb17d1fbfe775a953
neo <herbertdawkins@gmail.com>
2015-04-17 02:31:16 -0400

    simplified views by removing redundant context

--- demo/demosite/views.py
+++ demo/demosite/views.py
@@ -1,28 +1,12 @@
 from django.shortcuts import render
 
 def home(request):
-    context = {
-        "COMPANY_NAME": "EAT AT JOE'S",
-        "PHONE_NUMBER": "555-555-5555",
-        "COMPANY_SERVICE": "eat pizza",
-    }
-    return render(request, 'home.html', context)
+    return render(request, 'home.html', {})
 
 
 def about(request):
-    context = {
-        "COMPANY_NAME": "EAT AT JOE'S",
-        "PHONE_NUMBER": "555-555-5555",
-    }
-    return render(request, 'about.html', context)
+    return render(request, 'about.html', {})
 
 
 def contact(request):
-    context = {
-        "COMPANY_NAME": "EAT AT JOE'S",
-        "PHONE_NUMBER": "555-555-5555",
-        "STREET_ADDRESS": "555 NW 2000 Brick RD",
-        "CITY": "Wala Wala",
-        "STATE": "Vermont"
-    }
-    return render(request, 'contact.html', context)
+    return render(request, 'contact.html', {})
